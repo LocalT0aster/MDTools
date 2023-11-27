26d0846ec69ad41ca53a3082fb473b0c31ddb432
LocalT0aster <localt0aster@proton.me>
2023-11-27 16:51:50 +0300

    upd: Annotated Back Button

--- backButtonTree.py
+++ backButtonTree.py
@@ -1,16 +1,19 @@
 import os
 import re
 
-rootFolder = 'Course'
+rootFolder = '.'
 
 def button(pagePath: str):
     return f'\n[<kbd><br><- Return<br></kbd>]({pagePath})\n'
 
+def annotated_button(pagePath: str, pageName: str):
+    return f'\n[<kbd><br><- Return ({pageName})<br></kbd>]({pagePath})\n'
+
 # Step 1: Collect all links in each file
 links = {}  # Dictionary to store links found in each file
 
-# New regex to exclude return button links
-pattern = r'\[(?!<kbd><br><- Return<br></kbd>])[^]]+\]\((.*?\.md)\)'
+# Regex to exclude return button links
+pattern = r'\[(?!<kbd><br><- Return*<br></kbd>])[^]]+\]\((.*?\.md)\)'
 
 for (dirpath, dirnames, filenames) in os.walk(rootFolder):
     for name in filenames:
@@ -32,10 +35,10 @@ for file_path, linked_files in links.items():
         if os.path.exists(linked_file_path):
             relative_path = os.path.relpath(file_path, os.path.dirname(linked_file_path))
             # Calculate the relative path for the image from the linked file to Notes/img
-            back_button = button(relative_path)
+            back_button = annotated_button(relative_path, ' '.join(os.path.basename(relative_path).split('.')[:-1]))
             with open(linked_file_path, 'r') as f:
                 content = f.read()
                 # Check if the button is already there
-                if (back_button not in content) and not content.startswith('<!--Document-->'):
+                if ((back_button not in content) or (button(relative_path) not in content)) and not content.startswith('<!--Document-->'):
                     with open(linked_file_path, 'a') as f_append:
                         f_append.write(back_button)
